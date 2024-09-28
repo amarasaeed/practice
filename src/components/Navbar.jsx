3fa6043f78601264faad96dcac7c1029d5b9a70b
amarasaeed <amarasaeed787@gmail.com>
2024-09-29 00:09:52 +0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import './Navbar.css'
+
 
  function Navbar() {
   return (
     <div>
-        <nav className='nav-items'>
-            <ul>
-                
-            </ul>
+        <nav className='navbar'>
+          <img src='/images/favicon.ico'></img>
+            <div className='nav-items'>
+                <a>home</a>
+                <a>blog</a>
+                <a>contact</a>
+                <a>about</a>
+
+            </div>
         </nav>
     </div>
   )
