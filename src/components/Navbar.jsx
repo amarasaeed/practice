866cdc9d01be78ead64a06e83ad8025a45f31712
amarasaeed <amarasaeed787@gmail.com>
2024-09-28 23:25:58 +0500

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function Navbar() {
+  return (
+    <div>
+        <nav className='nav-items'>
+            <ul>
+                
+            </ul>
+        </nav>
+    </div>
+  )
+}
+export default Navbar
