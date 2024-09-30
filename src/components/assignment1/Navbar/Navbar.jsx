ea6bb18a2052070fdddb86036cd2c26b196a8302
amarasaeed <amarasaeed787@gmail.com>
2024-09-30 19:54:36 +0500

    navbar

--- src/components/assignment1/Navbar/Navbar.jsx
+++ src/components/assignment1/Navbar/Navbar.jsx
@@ -1,46 +1,28 @@
-// import React from 'react'
-// import './Navbar.css'
+import React from 'react'
+import './Navbar.css'
 
-
-//  function Navbar() {
-//   return (
-//     <div>
-//         <nav className='navbar'>
-//         <h1>
-//             <span className='chari'>Chari</span>
-//             <span className='team'>Team</span>
-//           </h1>
-//             <div className='nav-items'>
-//                 <a>home</a>
-//                 <a>page</a>
-//                 <a>blog</a>
-//                 <a>contact</a>
-//                 <a>about</a>
-//                 <a ><button>Donate Now <i className="fas fa-arrow-right"></i></button></a>
-                
-
-//             </div>
-//         </nav>
-//     </div>
-//   )
-// }
-// export default Navbar
-function nav(){
+function Navbar(){
   return(
-    <div>
+    <nav className='nav'>
       
         <h1 className="logo">
-          <span id="chari">Chari</span>
+          <span id="chari" >Chari</span>
           <span id="team">Team</span>
         </h1>
-   <div  className="nav-list">
+  
      <ul className="list-items">
-      <li>Home</li>
+      <li><a className='active'>Home</a></li>
       <li>About</li>
       <li>Causes</li>
-      <li>Pages</li>
+      <li className="dropdown">Pages<i className="fas fa-chevron-down fa-xs"></i>  <ul className="dropdown-content">
+        <li><a href="">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul></li>
       <li>Contact</li>
-    </ul></div>
-    </div>
+      <li id='button'>Donate Now <i className="fas fa-arrow-right"></i></li>
+    </ul>
+    </nav>
   )
 }
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import Navbar from './components/assignment1/Navbar/Navbar';
-import Hero from './components/assignment1/Hero/Hero';
-import Header from './components/assignment1/Header/Header';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <div>
-    <Header/>
-    <Navbar />
-    <Hero/>
+
+<Navbar/>
+   
     </div>
 );
 
